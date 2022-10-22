0e94eb14b58c7c59014c397add1fd8cd53ab1fe1
Sergio Pedri <sergio0694@live.com>
2022-10-22 23:53:40 +0200

    Add PolySharpExcludeGeneratedTypes, optimize generator

--- src/PolySharp.SourceGenerators/Extensions/AnalyzerConfigOptionsProviderExtensions.cs
+++ src/PolySharp.SourceGenerators/Extensions/AnalyzerConfigOptionsProviderExtensions.cs
@@ -3,6 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Immutable;
+using ComputeSharp.SourceGeneration.Helpers;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace PolySharp.SourceGenerators.Extensions;
@@ -25,5 +27,33 @@ internal static class AnalyzerConfigOptionsProviderExtensions
         return
             options.GlobalOptions.TryGetValue($"build_property.{propertyName}", out string? propertyValue) &&
             string.Equals(propertyValue, bool.TrueString, StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    /// Gets the value of an MSBuild property representing a semicolon-separated list of <see cref="string"/>-s.
+    /// </summary>
+    /// <param name="options">The input <see cref="AnalyzerConfigOptionsProvider"/> instance.</param>
+    /// <param name="propertyName">The MSBuild property name.</param>
+    /// <returns>The value of the specified MSBuild property.</returns>
+    public static ImmutableArray<string> GetStringArrayMSBuildProperty(this AnalyzerConfigOptionsProvider options, string propertyName)
+    {
+        if (options.GlobalOptions.TryGetValue($"build_property.{propertyName}", out string? propertyValue))
+        {
+            using ImmutableArrayBuilder<string> builder = ImmutableArrayBuilder<string>.Rent();
+
+            foreach (string part in propertyValue.Split(';'))
+            {
+                string trimmed = part.Trim();
+
+                if (trimmed.Length > 0)
+                {
+                    builder.Add(trimmed);
+                }
+            }
+
+            return builder.ToImmutable();
+        }
+
+        return ImmutableArray<string>.Empty;
     }
 }
---
+++ src/PolySharp.SourceGenerators/Models/GeneratedType.cs
@@ -0,0 +1,8 @@
+namespace PolySharp.SourceGenerators.Models;
+
+/// <summary>
+/// A model to hold information on a type to generate.
+/// </summary>
+/// <param name="FullyQualifiedMetadataName">The fully qualified type name of the type to generate.</param>
+/// <param name="IsPublicAccessibilityRequired">Whether to use public accessibility for the generated type.</param>
+internal sealed record GeneratedType(string FullyQualifiedMetadataName, bool IsPublicAccessibilityRequired);
