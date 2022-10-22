5f76a772ede3a9c4e15b2a340beae2b806da7881
Sergio Pedri <sergio0694@live.com>
2022-10-22 15:11:08 +0200

    Add tests for generated polyfills

---
+++ tests/PolySharp.Tests/LanguageFeatures.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace PolySharp.Tests;
+
+internal class TestClass
+{
+    private string? name;
+
+    // AllowNullAttribute
+    public void TakeValue1([AllowNull] string name)
+    {
+    }
+
+    // DisallowNullAttribute
+    public void TakeValue2([DisallowNull] string? name)
+    {
+    }
+
+    // DoesNotReturnAttribute
+    [DoesNotReturn]
+    public void Throws()
+    {
+        throw null!;
+    }
+
+    // DoesNotReturnIfAttribute
+    public void Throws([DoesNotReturnIf(true)] bool value)
+    {
+    }
+
+    // MaybeNullAttribute
+    [return: MaybeNull]
+    public string ModifyValue()
+    {
+        return null;
+    }
+
+    // MaybeNullWhenAttribute
+    public bool ModifyValue([MaybeNullWhen(true)] out string result)
+    {
+        result = null;
+
+        return true;
+    }
+
+    // MemberNotNullAttribute
+    [MemberNotNull(nameof(name))]
+    public void AssignsField()
+    {
+        this.name = "";
+    }
+
+    // MemberNotNullWhenAttribute
+    [MemberNotNullWhen(true, nameof(name))]
+    public bool ConditionallyAssignsField()
+    {
+        this.name = "";
+
+        return true;
+    }
+
+    // NotNullAttribute
+    public void TakeValue3([NotNull] string? value)
+    {
+        throw null!;
+    }
+
+    // NotNullIfNotNullAttribute
+    [return: NotNullIfNotNull(nameof(value))]
+    public string? TakeValue4(string? value)
+    {
+        return value;
+    }
+
+    // NotNullWhenAttribute
+    public bool TakeValue([NotNullWhen(true)] out string? value)
+    {
+        value = this.name ?? "";
+
+        return true;
+    }
+}
+
+internal class TestClassWithRequiredMembers
+{
+    // SetsRequiredMembersAttribute
+    [SetsRequiredMembers]
+    public TestClassWithRequiredMembers()
+    {
+        Name = "";
+    }
+
+    // CompilerFeatureRequiredAttribute, RequiredMemberAttribute
+    public required string Name { get; init; }
+}
+
+// IsExternalInit
+internal record Person(string Name);
+
+internal readonly struct TestStruct
+{
+    private readonly int number;
+
+    // UnscopedRefAttribute
+    [UnscopedRef]
+    public readonly ref readonly int GetRef()
+    {
+        return ref this.number;
+    }
+}
+
+internal static class IndexAndRangeTests
+{
+    // Index
+    public static int TestIndex(ReadOnlySpan<int> numbers)
+    {
+        return numbers[^1];
+    }
+
+    // Range
+    public static ReadOnlySpan<int> TestRange(ReadOnlySpan<int> numbers)
+    {
+        return numbers[1..^4];
+    }
+}
+
+internal class AnotherTestClass
+{
+    // CallerArgumentExpressionAttribute
+    public string AutomaticName(int number, [CallerArgumentExpression(nameof(number))] string name = "")
+    {
+        return name;
+    }
+
+    [SkipLocalsInit]
+    public void Stackalloc()
+    {
+        _ = stackalloc int[8];
+    }
+
+    public void Handler(string name, [InterpolatedStringHandlerArgument(nameof(name))] ref TestHandler handler)
+    {
+    }
+}
+
+[InterpolatedStringHandler]
+internal struct TestHandler
+{
+}
